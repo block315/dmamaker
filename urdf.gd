d5d366f18d0820546205333e51433b9e8db8cf8c
block315 <block315ccc@gmail.com>
2025-12-30 12:46:54 +0900

    feat: basic urdf file import

--- urdf.gd
+++ urdf.gd
@@ -4,12 +4,13 @@ extends Node
 
 func parser_to_node(parser: XMLParser):
 	var _xml_node_stack = []
-	var node_index = 0
-	var _node_stack = []
+	var _node_index = 1
+	var _new_node_name = {}
 	var _connection: String
 	var _connection_name: String
 	var _dma_node: DMANode
-	var _geometry_for_visual = true
+	var _joint_type: String
+	var _parent: String
 	while parser.read() != ERR_FILE_EOF:
 		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
 			_xml_node_stack.append(parser.get_node_name())
@@ -20,7 +21,8 @@ func parser_to_node(parser: XMLParser):
 			and parser.get_node_name() == "link":
 			_dma_node = DMANode.new()
 			_dma_node.resizable = true
-			_dma_node.description = parser.get_named_attribute_value("name")
+			_dma_node.name = parser.get_named_attribute_value("name") + (ProgramConfig.index_split_symbol + str(_node_index))
+			_node_index += 1
 			for _ports in ["revolute", "continuous", "prismatic", "fixed", "floating", "planar"]:
 				var _label = Label.new()
 				_label.name = _ports
@@ -28,3 +30,17 @@ func parser_to_node(parser: XMLParser):
 				_dma_node.add_child(_label)
 				_dma_node.set_slot_enabled_right(_label.get_index(), true)
 			graph_edit.add_child(_dma_node)
+			_new_node_name[parser.get_named_attribute_value("name")] = _dma_node.name
+	parser.seek(0)
+	while parser.read() != ERR_FILE_EOF:
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT \
+			and parser.get_node_name() == "joint":
+				_joint_type = parser.get_named_attribute_value("type")
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT \
+			and parser.get_node_name() == "parent":
+				_parent = parser.get_named_attribute_value("link")
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT \
+			and parser.get_node_name() == "child":
+				graph_edit.connect_node(
+					_new_node_name[_parent], graph_edit.get_node(NodePath(_new_node_name[_parent]+"/"+_joint_type)).get_index(),\
+					_new_node_name[parser.get_named_attribute_value("link")], 0)
